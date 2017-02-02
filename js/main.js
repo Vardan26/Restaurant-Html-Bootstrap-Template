6a342df17a6447ef10bba603089333d030aa4411
Vardan <vard.1995@gmail.com>
2017-02-02 20:54:37 +0400

    Adds template

---
+++ js/main.js
@@ -0,0 +1,19 @@
+$('#slider--1').slick({
+   dots:true
+});
+
+$('#ourTeam__slider').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1
+
+});
+
+$('#gallery__slider').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    dots:true,
+    autoplay:true
+});
+$('#testimonial__slider').slick({
+    dots:true
+});
